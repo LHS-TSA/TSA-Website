bf22dfdf01a7d073bfbff8be11bd57faea6ebdb3
Stephen DuVall <stephen.duvall13@gmail.com>
2017-11-16 08:51:55 -0500

    Work that I did in prison

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ function generateMembers(person) {
   var master = $(template);
   master.find(".card-img-top").attr("src", "img/" + person.img);
   master.find(".card-title").text(person.name);
+  master.find(".card-subtitle").text(person.position);
   master.find(".card-text").text('"' + person.quote + '"');
   $("#members").append(master);
 
@@ -13,18 +14,23 @@ var template = '<div id="template" class="col-md-4 col-sm-12 my-3">' +
 '<img class="card-img-top p-3" src="">' +
 '<div class="card-body p-3">' +
 '<h4 class="card-title"></h4>' +
+'<h5 class="card-subtitle"></h5>' + 
 '<p class="card-text"></p>' +
 '</div>' +
 '</div>' +
 '</div>';
 
-
-var member = {
-  img: "blank_img2.svg",
-  name: "Stephen DuVall",
-  quote: "I probably didn't turn it in2",
-};
+var members = [
+  { name:"Stephen DuVall", position:"Reporter - Webmaster", quote:"I probably didn't turn it in", img:"blank_img2.svg" },
+  { name:"Skylar Mack", position:"Member", quote:"It's not my fault...", img:"blank_img.svg" },
+  { name:"Weston Allred", position:"VEX Team Captian", quote:"I saw this thing on the VEX Forum", img:"blank_img2.svg"},
+  { name:"Jacob", position:"President -_-", quote:"...", img:"blank_img.svg"},
+  { name:"Riley Dunn", position:"Member - Cheerleader", quote:"Goodbye Coach Tompkins!!!", img:"blank_img2.svg"},
+];
 
 $(document).ready(function() {
-  generateMembers(member);
+  for(var i = 0; i < members.length; i++) {
+    generateMembers(members[i]);
+    console.log(members[i].name);
+  }
 });
---
+++ js/testing.js
@@ -0,0 +1,7 @@
+// var members = [
+//     { name:"Stephen DuVall", quote:"I probably didn't turn it in", img:"blank_img2.svg" },
+//     { name:"Skylar Mack", quote:"It's not my fault...", img:"blank_img.svg" },
+//     { name:"Weston Allred", quote:"I saw this thing on the VEX Forum", img:"blank_img2.svg"},
+//     { name:"umm what", quote:"...", img:"blank_img.svg"},
+//     { name:"Riley Dunn", quote:"Goodbye Coach Tompkins!!!", img:"blank_img2.svg"},
+//   ]
