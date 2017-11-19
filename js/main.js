f6ce4ddbdc2f05e46c25cc4371e13b28edf02701
Stephen DuVall <stephen.duvall13@gmail.com>
2017-11-19 16:39:46 -0500

    edited card generator and card design

--- js/main.js
+++ js/main.js
@@ -1,25 +1,43 @@
 // JavaScript Document
-function generateCards(person, place) {
-      var master = $(template);
-  master.find(".card-img-top").attr("src", "img/" + person.img);
-  master.find(".card-title").text(person.name);
-  master.find(".card-subtitle").text(person.position);
-  master.find(".card-text").text('"' + person.quote + '"');
-      $(place).append(master);
-}
-
+function generateCards(group, place) {
+  switch (place) {
+    case "#officers":
       var template =
-  '<div id="template" class="col-md-3 col-sm-10 my-3">' +
+          '<div class="col-md-3 col-sm-10 my-3">' +
           '<div class="card">' +
-  '<img class="card-img-top p-3" src="">' +
+          '<img class="card-img-top p-3" src="">' +
           '<div class="card-body p-3">' +
-          '<h4 class="card-title"></h4>' +
           '<h5 class="card-subtitle"></h5>' +
-  '<p class="card-text"></p>' +
-  "</div>" +
+          '<h4 class="card-title"></h4>' +
+          '<p class="card-text my-1"></p>' +
+          "</div>" +
           "</div>" +
           "</div>";
 
+      var master = $(template);
+      master.find(".card-img-top").attr("src", "img/" + group.img);
+      master.find(".card-title").text(group.name);
+      master.find(".card-subtitle").text(group.position);
+      master.find(".card-text").text('"' + group.quote + '"');
+      $(place).append(master);
+      break;
+
+    case "#members":
+      var template =
+          '<div class="card">' +
+          '<div class="card-body p-3">' +
+          '<h4 class="card-title"></h4>' +
+          '<h5 class="card-subtitle"></h5>' +
+          '<p class="card-text my-1"></p>' +
+          "</div>" +
+          "</div>";
+
+      var master = $(template);
+      master.find(".card-title").text(group.name);
+      master.find(".card-subtitle").text(group.projects);
+      master.find(".card-text").text('"' + group.quote + '"');
+      $(place).append(master);
+      break;
   }
 }
 
