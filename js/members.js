5439d673b9c4361fab5791c26330d8453f307513
Stephen DuVall <stephen.duvall13@gmail.com>
2017-11-19 16:38:51 -0500

    Moved JSON data to members.js

---
+++ js/members.js
@@ -0,0 +1,113 @@
+var members = [
+  {
+    name: "Weston Allred",
+    projects: "VEX Robotics, Engineering design",
+    quote: "I found this cool thing on the VEX forum"
+  },
+  {
+    name: "Riley Dunn",
+    projects: "Fashion Design",
+    quote: "This is my club"
+  },
+  {
+    name: "Josie Haulk",
+    projects: "Future Tech Teacher",
+    quote: "I'm a little late."
+  },
+  {
+    name: "Alex Stone",
+    projects: "Transportation Modeling",
+    quote: "Hi Skylar"
+  },
+  {
+    name: "Peter Phan",
+    projects: null,
+    quote: "I'm tired."
+  },
+  {
+    name: "Maxwell Turcotte",
+    projects: "VEX Robotics, Flight Endurance, Coding",
+    quote: "I don't heckin' know"
+  },
+  {
+    name: "Jeremy Mullis",
+    projects: "VEX Robotics, Flight Endurance",
+    quote: "oof"
+  },
+  {
+    name: "Athni Tesfaye",
+    projects: "VEX VEX Robotics, Digital Video Production",
+    quote: "My legs are sore"
+  },
+  {
+    name: "Scott Askew",
+    projects: "Flight Endurance",
+    quote: "To the future, the past, and everything in between!"
+  },
+  {
+    name: "Skylar Mack",
+    projects: "Structural",
+    quote: "Hi Alex"
+  },
+  {
+    name: "Kevin Pietruszka",
+    projects: "VEX Robotics",
+    quote: "It's all Riley's fault and Skylar is most definitely wrong!"
+  },
+  {
+    name: "Averi Allred",
+    projects: "Architectural Design",
+    quote: "Yeah, but that's not good enough."
+  },
+  {
+    name: "Joshua D. Seidenberger",
+    projects: "Future Tech Teacher, Technology Problem Solving",
+    quote: "You don't need a license to drive a sandwich."
+  },
+  {
+    name: "Spencer Burch",
+    projects: "Transportation Modeling, Video Production ",
+    quote: "What do I desire?",
+  },
+  {
+    name: "William Greene",
+    projects: "Video Production",
+    quote: "Get it together guys."
+  },
+  {
+    name: "Ben Smith",
+    projects: "Music Production",
+    quote: "Is Mayonnaise an instrument?"
+  }
+];
+
+var officers = [
+  {
+    name: "Jacob  Smith",
+    position: "President",
+    projects: "Flight endurance, Structural design, Video game design",
+    quote: "If life gives you lemons make life take the lemons back.",
+    img: "blank_img.svg"
+  },
+  {
+    name: "Debora Park",
+    position: "Secretary",
+    projects: "Music Production, Flight Endurance",
+    quote: "I'm blind.",
+    img: "blank_img.svg"
+  },
+  {
+    name: "Devon Loy",
+    position: "Vice President",
+    projects: "Dragster Design, Video Game Design, Debate",
+    quote: "For every fixed syntax error, there are always ten more.",
+    img: "blank_img.svg"
+  },
+  {
+    name: "Stephen DuVall",
+    position: "Reporter",
+    projects: "Webmaster, Software Dev, VEX Robotics",
+    quote: "I probably didn't turn it in",
+    img: "blank_img.svg"
+  }
+];
