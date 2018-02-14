e293ee58627d6ee9551b0bdf3be3b12a6cf704e9
Stephen DuVall <stephen.duvall13@gmail.com>
2018-02-14 16:15:43 -0500

    Began adding images to actual content to posts

--- js/posts.js
+++ js/posts.js
@@ -1,25 +1,31 @@
 var posts = [
     {
-      title: "VEX Competition",
-      img: "IMG_9326.jpg",
+      title: "Clarkston VEX Tournament",
+      img: "IMG_2064.jpg",
       body:
-        "Robotics team members Weston, Stephen, Jermey, Athini, Max, Kevin, and John competed this weekend at Clarkston High School and took home 4th place after competing all the way to semfininals.",
+        "Robotics team members Weston, Stephen, Jeremy, Athini, Max, Kevin, and John competed this weekend at Clarkston High School and took home 4th place after competing all the way to semfininals.",
       date: "Feb. 5, 2018",
       source: "stphn"
     },
     {
-      title: "VEX Clarkston Tournament",
-      img: "IMG_2064.JPG",
-      body: "Second Post",
+      title: "Walnut Grove VEX Tournament",
+      img: "IMG_1962.jpg",
+      body: "Weston, Stephen, Jeremy, John, Athini, Kevin, and Brett competed with two robots down the streat at Walnut Grove High School",
       date: "Feb. 5, 2018",
       source: "stphn"
     },
+    {
+      title: "West Forsyth HS VEX Tournament",
+      img: "IMG_0250.jpg",
+      body: "This weekend Weston, Stephen, Jeremy, and Athini competed at a robotics competition placing 8th overall making it to the first round of quarter finals",
+      date: "Nov. 11, 2017",
+      source: "stphn"
+    },
       {
-      title: "Posts",
-      img: "blank_img3.svg",
-      body: "If you would like to submit a post, please fill out this form and then notify me that you have done so.",
-      date: "Feb. 6, 2018",
+      title: "Tech Day",
+      img: "IMG_1368.jpg",
+      body: "Our members competed during Tech Day this year at the Perry Fair Grounds. Weston Allred & Jermey Mullis took second place with their \"full pull\" robot. The chapter also competed in: Membership Recruitment, Alternative Energy Design, Mousetrap Car Challenge, and Structural Design.",
+      date: "Oct. 9, 2017",
       source: "stphn"
     },
   ];
-  
