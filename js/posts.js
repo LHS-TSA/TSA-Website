1da246753ff2acd7786e00ddec6c68648367e378
Stephen DuVall <stephen.duvall13@gmail.com>
2018-02-19 17:08:57 -0500

    Fixed Athni's name

--- js/posts.js
+++ js/posts.js
@@ -9,30 +9,29 @@ var posts = [
   {
     title: "Clarkston VEX Tournament",
     img: "IMG_2064.jpg",
-      body:
-        "Robotics team members Weston, Stephen, Jeremy, Athini, Max, Kevin, and John competed this weekend at Clarkston High School and took home 4th place after competing all the way to semfininals.",
+    body: "Robotics team members Weston, Stephen, Jeremy, Athni, Max, Kevin, and John competed this weekend at Clarkston High School and took home 4th place after competing all the way to semifininals.",
     date: "Feb. 5, 2018",
     source: "stphn"
   },
   {
     title: "Walnut Grove VEX Tournament",
     img: "IMG_1962.jpg",
-      body: "Weston, Stephen, Jeremy, John, Athini, Kevin, and Brett competed with two robots down the street at Walnut Grove High School.",
+    body: "Weston, Stephen, Jeremy, John, Athni, Kevin, and Brett competed with two robots down the street at Walnut Grove High School.",
     date: "Feb. 5, 2018",
     source: "stphn"
   },
   {
     title: "West Forsyth HS VEX Tournament",
     img: "IMG_0250.jpg",
-      body: "This weekend Weston, Stephen, Jeremy, and Athini competed at a robotics competition placing 8th overall making it to the first round of quarter finals.",
+    body: "This weekend Weston, Stephen, Jeremy, and Athni competed at a robotics competition placing 8th overall making it to the first round of quarter finals.",
     date: "Nov. 11, 2017",
     source: "stphn"
   },
-      {
-      title: "Tech Day",
-      img: "IMG_1368.jpg",
-      body: "Our members competed during Tech Day this year at the Perry Fair Grounds. Weston Allred & Jermey Mullis took second place with their \"full pull\" robot. The chapter also competed in: Membership Recruitment, Alternative Energy Design, Mousetrap Car Challenge, and Structural Design.",
-      date: "Oct. 9, 2017",
-      source: "stphn"
-    },
-  ];
+    {
+    title: "Tech Day",
+    img: "IMG_1368.jpg",
+    body: "Our members competed during Tech Day this year at the Perry Fair Grounds. Weston Allred & Jermey Mullis took second place with their \"full pull\" robot. The chapter also competed in: Membership Recruitment, Alternative Energy Design, Mousetrap Car Challenge, and Structural Design.",
+    date: "Oct. 9, 2017",
+    source: "stphn"
+  },
+];
