b5ecc804ab8fa5264bf95cfeb118de32f6825b30
Stephen DuVall <stephen.duvall13@gmail.com>
2018-02-19 17:08:41 -0500

    Added post for Arabia Mountain

--- js/posts.js
+++ js/posts.js
@@ -1,26 +1,33 @@
 var posts = [
-    {
-      title: "Clarkston VEX Tournament",
-      img: "IMG_2064.jpg",
+  {
+    title: "Arabia Mountain VEX competition",
+    img: "IMG_8249.jpg",
+    body: "Weston, Stephen, Jeremy, Max, Kevin, Athni, and Brett had an amazing competition at Arabia High School this weekend. The team was undefeated throughout all of their qualification matches and won second overall, securing a ticket to state.",
+    date: "Feb. 17, 2018",
+    source: "stphn"
+  },
+  {
+    title: "Clarkston VEX Tournament",
+    img: "IMG_2064.jpg",
       body:
         "Robotics team members Weston, Stephen, Jeremy, Athini, Max, Kevin, and John competed this weekend at Clarkston High School and took home 4th place after competing all the way to semfininals.",
-      date: "Feb. 5, 2018",
-      source: "stphn"
-    },
-    {
-      title: "Walnut Grove VEX Tournament",
-      img: "IMG_1962.jpg",
+    date: "Feb. 5, 2018",
+    source: "stphn"
+  },
+  {
+    title: "Walnut Grove VEX Tournament",
+    img: "IMG_1962.jpg",
       body: "Weston, Stephen, Jeremy, John, Athini, Kevin, and Brett competed with two robots down the street at Walnut Grove High School.",
-      date: "Feb. 5, 2018",
-      source: "stphn"
-    },
-    {
-      title: "West Forsyth HS VEX Tournament",
-      img: "IMG_0250.jpg",
+    date: "Feb. 5, 2018",
+    source: "stphn"
+  },
+  {
+    title: "West Forsyth HS VEX Tournament",
+    img: "IMG_0250.jpg",
       body: "This weekend Weston, Stephen, Jeremy, and Athini competed at a robotics competition placing 8th overall making it to the first round of quarter finals.",
-      date: "Nov. 11, 2017",
-      source: "stphn"
-    },
+    date: "Nov. 11, 2017",
+    source: "stphn"
+  },
       {
       title: "Tech Day",
       img: "IMG_1368.jpg",
